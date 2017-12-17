a721b066bda11a81fd68f19543a8cb4e7d59580f
Mehmet Melik Kose <mekose@opendns.com>
2017-12-17 02:11:35 +0000

    code clean up.

--- twitter-secretary/src/main/java/tr/edu/boun/secretary/twitter/TwitterTimelineController.java
+++ twitter-secretary/src/main/java/tr/edu/boun/secretary/twitter/TwitterTimelineController.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package tr.edu.boun.secretary.twitter;
 
 import javax.inject.Inject;
@@ -41,14 +26,19 @@ public class TwitterTimelineController {
 	
 	@RequestMapping(value="/twitter/timeline/{timelineType}", method=RequestMethod.GET)
 	public String showTimeline(@PathVariable("timelineType") String timelineType, Model model) {
-		if (timelineType.equals("Home")) {
-			model.addAttribute("timeline", twitter.timelineOperations().getHomeTimeline());
-		} else if(timelineType.equals("User")) {
-			model.addAttribute("timeline", twitter.timelineOperations().getUserTimeline());
-		} else if(timelineType.equals("Mentions")) {
-			model.addAttribute("timeline", twitter.timelineOperations().getMentions());
-		} else if(timelineType.equals("Favorites")) {
-			model.addAttribute("timeline", twitter.timelineOperations().getFavorites());
+		switch (timelineType) {
+			case "Home":
+				model.addAttribute("timeline", twitter.timelineOperations().getHomeTimeline());
+				break;
+			case "User":
+				model.addAttribute("timeline", twitter.timelineOperations().getUserTimeline());
+				break;
+			case "Mentions":
+				model.addAttribute("timeline", twitter.timelineOperations().getMentions());
+				break;
+			case "Favorites":
+				model.addAttribute("timeline", twitter.timelineOperations().getFavorites());
+				break;
 		}
 		model.addAttribute("timelineName", timelineType);
 		return "twitter/timeline";
